ed5048ff4ad39cf2a266a3a09aa5c387528ed70c
shai <sexbon@gmail.com>
2024-11-12 21:13:39 +0200

    Initial commit

---
+++ lesson1.js
@@ -0,0 +1,8 @@
+var luckyNumber = 7;
+var luckyNumberstr = '7';
+
+// dont use ever  double == 
+console.log(luckyNumberstr==luckyNumber);
+
+// else three ===
+console.log(luckyNumberstr=== luckyNumber);
